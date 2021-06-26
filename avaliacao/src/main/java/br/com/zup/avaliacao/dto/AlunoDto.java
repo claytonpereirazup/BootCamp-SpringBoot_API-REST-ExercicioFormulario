18ffecdb6db225058a1ddc910887d4050033f555
Clayton Pereira <clayton.pereira@zup.com.br>
2021-06-26 14:25:03 -0300

    Project - Aluno DTO Created

---
+++ avaliacao/src/main/java/br/com/zup/avaliacao/dto/AlunoDto.java
@@ -0,0 +1,30 @@
+package br.com.zup.avaliacao.dto;
+
+public class AlunoDto {
+	
+	private String nome;
+	private Integer idade;
+	private String email;
+	
+	public AlunoDto() {
+	}
+
+	public AlunoDto(String nome, Integer idade, String email) {
+		this.nome = nome;
+		this.idade = idade;
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+}
--- avaliacao/src/main/java/br/com/zup/avaliacao/form/AlunoForm.java
+++ avaliacao/src/main/java/br/com/zup/avaliacao/form/AlunoForm.java
@@ -14,14 +14,32 @@ public class AlunoForm {
 	@Size(min = 15, max = 30, message = "Deve estar entre 10 e 30 Caracteres")
 	private String email;
 	
+	public AlunoForm() {
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public Integer getIdade() {
 		return idade;
 	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
 	public String getEmail() {
 		return email;
 	}
 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+		
+	
 }
