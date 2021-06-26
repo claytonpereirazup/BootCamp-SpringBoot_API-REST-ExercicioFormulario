85c5b66a05025f02e5016deb6ad7d072ea414a98
Clayton Pereira <clayton.pereira@zup.com.br>
2021-06-26 14:13:46 -0300

    Project - Aluno Entity Created

---
+++ avaliacao/src/main/java/br/com/zup/avaliacao/modelo/Aluno.java
@@ -0,0 +1,87 @@
+package br.com.zup.avaliacao.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_aluno")
+public class Aluno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	@Column(columnDefinition = "TINYINT(100)")
+	private Integer idade;
+	private String email;
+	
+	public Aluno() {
+	}
+	
+	public Aluno(Long id, String nome, Integer idade, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+		this.email = email;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Aluno [id=" + id + ", nome=" + nome + ", idade=" + idade + ", email=" + email + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
